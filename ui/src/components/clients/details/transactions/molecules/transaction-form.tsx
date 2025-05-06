9b4bda73e3bde56ba88b0fa7ab40f6bcfb6474e9
Caciuleanu Ciprian <caciuleanu.ciprian@gmail.com>
2025-05-06 19:43:58 +0300

    checkbox fix

--- ui/src/components/clients/details/transactions/molecules/transaction-form.tsx
+++ ui/src/components/clients/details/transactions/molecules/transaction-form.tsx
@@ -1,31 +1,30 @@
 import InputWithLabel from "@/components/common/forms/input-with-label";
-import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { LanguageContext } from "@/i18n/language-context";
 import { Form, Formiz } from "@formiz/core";
-import { SetStateAction, useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import {
-  fileAtom,
-  transactionExpiryDateAtom,
-  transactionTypeSelectAtom,
+	fileAtom,
+	transactionExpiryDateAtom,
+	transactionTypeSelectAtom,
 } from "@/components/clients/details/transactions/utils/transactions.recoil";
 import {
-  Select,
-  SelectTrigger,
-  SelectValue,
-  SelectContent,
-  SelectItem,
+	Select,
+	SelectTrigger,
+	SelectValue,
+	SelectContent,
+	SelectItem,
 } from "@/components/ui/select";
 import {
-  isMinNumber,
-  isNotEmptyString,
-  isNumber,
-  isRequired,
+	isMinNumber,
+	isNotEmptyString,
+	isNumber,
+	isRequired,
 } from "@formiz/validations";
 import {
-  selectTransactionsTypeOptions,
-  valueToLabelTransactionType,
+	selectTransactionsTypeOptions,
+	valueToLabelTransactionType,
 } from "../utils/consts";
 import { TransactionObject, TransactionType } from "../utils/types";
 import { UploadFiles } from "@/components/common/settings/molecules/upload-image";
@@ -33,127 +32,129 @@ import { CheckboxWithText } from "@/components/common/forms/checkbox-with-text";
 import { DatePicker } from "@/components/common/date-picker";
 
 interface TransactionFormProps {
-  form: Form;
-  data?: TransactionObject;
-  isEdit?: boolean;
+	form: Form;
+	data?: TransactionObject;
+	isEdit?: boolean;
 }
 
 export const TransactionForm = ({
-  form,
-  data,
-  isEdit = false,
+	form,
+	data,
+	isEdit = false,
 }: TransactionFormProps) => {
-  const [files, setFiles] = useRecoilState(fileAtom);
-  const [transactionExpiryDate, setTransactionExpiryDate] = useRecoilState(
-    transactionExpiryDateAtom
-  );
-  const [, setTransactionType] = useRecoilState(transactionTypeSelectAtom);
-  const [shouldDisplayDatePicker, setShouldDisplayDatePicker] = useState(
-    data?.expiryDate ? true : false
-  );
+	const [files, setFiles] = useRecoilState(fileAtom);
+	const [transactionExpiryDate, setTransactionExpiryDate] = useRecoilState(
+		transactionExpiryDateAtom,
+	);
+	const [, setTransactionType] = useRecoilState(transactionTypeSelectAtom);
+	const [shouldDisplayDatePicker, setShouldDisplayDatePicker] = useState(
+		data?.expiryDate ? true : false,
+	);
 
-  const { dictionary } = useContext(LanguageContext);
+	const { dictionary } = useContext(LanguageContext);
 
-  useEffect(() => {
-    if (data) {
-      setTransactionType(data.transactionType);
-    } else {
-      setTransactionType(TransactionType.INCOME);
-    }
-  }, [data]);
+	useEffect(() => {
+		if (data) {
+			setTransactionType(data.transactionType);
+		} else {
+			setTransactionType(TransactionType.INCOME);
+		}
+	}, [data]);
 
-  useEffect(() => {
-    setFiles(null);
-    setTransactionExpiryDate(undefined);
-  }, []);
+	useEffect(() => {
+		setFiles(null);
+		setTransactionExpiryDate(undefined);
+	}, []);
 
-  useEffect(() => {
-    form.setValues({
-      ...data,
-      expiryDate: data?.expiryDate,
-      transactionType: data?.transactionType,
-    });
-  }, [data]);
+	useEffect(() => {
+		form.setValues({
+			...data,
+			expiryDate: data?.expiryDate,
+			transactionType: data?.transactionType,
+		});
+	}, [data]);
 
-  return (
-    <Formiz connect={form}>
-      <div className="col-span-4 w-full">
-        <InputWithLabel
-          label={dictionary.Name}
-          type={"text"}
-          name={"name"}
-          required={dictionary.FieldCannotBeEmpty}
-          validations={[
-            {
-              handler: isRequired() && isNotEmptyString(),
-              message: `${dictionary.InvalidName}`,
-            },
-          ]}
-        />
-      </div>
-      <div className="col-span-4 w-full">
-        <InputWithLabel
-          label={dictionary.Amount}
-          type={"number"}
-          name={"amount"}
-          required={dictionary.FieldCannotBeEmpty}
-          validations={[
-            {
-              handler: isRequired() && isNumber() && isMinNumber(0),
-              message: `${dictionary.InvalidAmount}`,
-            },
-          ]}
-        />
-      </div>
-      <div className="col-span-4 w-full pb-4">
-        <Select
-          onValueChange={(e: any) => setTransactionType(e)}
-          defaultValue={data?.transactionType}
-        >
-          <Label>{dictionary.TransactionType}</Label>
-          <SelectTrigger>
-            <SelectValue placeholder={dictionary.Income} />
-          </SelectTrigger>
-          <SelectContent>
-            {selectTransactionsTypeOptions.map((option: string) => (
-              <SelectItem key={option} value={option}>
-                {valueToLabelTransactionType(option, dictionary)}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
-      </div>
-      {!isEdit && (
-        <>
-          <div className="col-span-4 w-full">
-            <CheckboxWithText
-              checked={shouldDisplayDatePicker}
-              onCheckedChange={setShouldDisplayDatePicker}
-              label={dictionary.DisplayDatePicker}
-            />
-          </div>
-          {shouldDisplayDatePicker && (
-            <div className="col-span-4 w-full">
-              <DatePicker
-                date={transactionExpiryDate}
-                setDate={setTransactionExpiryDate}
-                dateFormat="dd MMM yyyy"
-                full={true}
-              />
-            </div>
-          )}
-        </>
-      )}
+	return (
+		<Formiz connect={form}>
+			<div className="col-span-4 w-full">
+				<InputWithLabel
+					label={dictionary.Name}
+					type={"text"}
+					name={"name"}
+					required={dictionary.FieldCannotBeEmpty}
+					validations={[
+						{
+							handler: isRequired() && isNotEmptyString(),
+							message: `${dictionary.InvalidName}`,
+						},
+					]}
+				/>
+			</div>
+			<div className="col-span-4 w-full">
+				<InputWithLabel
+					label={dictionary.Amount}
+					type={"number"}
+					name={"amount"}
+					required={dictionary.FieldCannotBeEmpty}
+					validations={[
+						{
+							handler: isRequired() && isNumber() && isMinNumber(0),
+							message: `${dictionary.InvalidAmount}`,
+						},
+					]}
+				/>
+			</div>
+			<div className="col-span-4 w-full pb-4">
+				<Select
+					onValueChange={(e: any) => setTransactionType(e)}
+					defaultValue={data?.transactionType}
+				>
+					<Label>{dictionary.TransactionType}</Label>
+					<SelectTrigger>
+						<SelectValue placeholder={dictionary.Income} />
+					</SelectTrigger>
+					<SelectContent>
+						{selectTransactionsTypeOptions.map((option: string) => (
+							<SelectItem key={option} value={option}>
+								{valueToLabelTransactionType(option, dictionary)}
+							</SelectItem>
+						))}
+					</SelectContent>
+				</Select>
+			</div>
+			{!isEdit && (
+				<>
+					<div className="col-span-4 w-full">
+						<CheckboxWithText
+							checked={shouldDisplayDatePicker}
+							onCheckedChange={() =>
+								setShouldDisplayDatePicker(!shouldDisplayDatePicker)
+							}
+							label={dictionary.DisplayDatePicker}
+						/>
+					</div>
+					{shouldDisplayDatePicker && (
+						<div className="col-span-4 w-full">
+							<DatePicker
+								date={transactionExpiryDate}
+								setDate={setTransactionExpiryDate}
+								dateFormat="dd MMM yyyy"
+								full={true}
+							/>
+						</div>
+					)}
+				</>
+			)}
 
-      {/* TODO Add validation to this filed */}
-      {!data && (
-        <UploadFiles
-          setFiles={setFiles}
-          files={files ?? []}
-          isReadonly={false}
-          accept={{}}
-        />
-      )}
-    </Formiz>
-  );
+			{/* TODO Add validation to this filed */}
+			{!data && (
+				<UploadFiles
+					setFiles={setFiles}
+					files={files ?? []}
+					isReadonly={false}
+					accept={{}}
+				/>
+			)}
+		</Formiz>
+	);
 };
--- ui/src/components/common/forms/checkbox-with-text.tsx
+++ ui/src/components/common/forms/checkbox-with-text.tsx
@@ -6,64 +6,69 @@ import React from "react";
 import { InputCheckbox } from "./input";
 
 interface CheckboxWithTextProps {
-  name?: string;
-  label: string;
-  sublabel?: string;
-  checked?: boolean;
-  onCheckedChange?: (params?: any) => void;
+	name?: string;
+	label: string;
+	sublabel?: string;
+	checked?: boolean;
+	onCheckedChange?: (params?: any) => void;
 }
 
 export function CheckboxWithText({
-  name,
-  label,
-  sublabel,
-  checked,
-  onCheckedChange,
+	name,
+	label,
+	sublabel,
+	checked,
+	onCheckedChange,
 }: CheckboxWithTextProps) {
-  const [value, setValue] = React.useState<boolean>(checked || false);
-  return (
-    <div className="items-top flex space-x-2">
-      <Checkbox
-        id={name}
-        name={name}
-        checked={checked || value}
-        onCheckedChange={(checked) =>
-          onCheckedChange ? onCheckedChange(!checked) : setValue(!checked)
-        }
-      />
-      <div className="grid gap-1.5 leading-none">
-        <label
-          htmlFor={name}
-          className="text-sm  leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-        >
-          {label}
-        </label>
-        <p className="text-sm text-muted-foreground">{sublabel}</p>
-      </div>
-    </div>
-  );
+	console.log(name, label, sublabel, checked, onCheckedChange);
+
+	const handleCheckedChange = (checked: boolean) => {
+		if (onCheckedChange) {
+			onCheckedChange(checked);
+		}
+	};
+
+	return (
+		<div className="items-top flex space-x-2">
+			<Checkbox
+				id={name}
+				name={name}
+				checked={checked}
+				onCheckedChange={(checked) => handleCheckedChange(!checked)}
+			/>
+			<div className="grid gap-1.5 leading-none">
+				<label
+					htmlFor={name}
+					className="text-sm  leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+				>
+					{label}
+				</label>
+				<p className="text-sm text-muted-foreground">{sublabel}</p>
+			</div>
+		</div>
+	);
 }
 
 type CheckboxWithTextPropsForm = {
-  name: string;
-  label: string;
-  sublabel?: string;
-  checked?: boolean;
-  onCheckedChange?: (params?: any) => void;
-  isLoading?: boolean;
-  isDisabled?: boolean;
-  defaultValue?: string;
-  placeholder?: string;
+	name: string;
+	label: string;
+	sublabel?: string;
+	checked?: boolean;
+	onCheckedChange?: (params?: any) => void;
+	isLoading?: boolean;
+	isDisabled?: boolean;
+	defaultValue?: string;
+	placeholder?: string;
 };
 
 export function CheckboxInputWithText(props: CheckboxWithTextPropsForm) {
-  const { label, name, ...rest } = props;
-  return (
-    <div className="flex items-center gap-2">
-      <InputCheckbox {...props} />
-      <Label htmlFor={name} className="text-sm">
-        {label}
-      </Label>
-    </div>
-  );
+	const { label, name, ...rest } = props;
+	return (
+		<div className="flex items-center gap-2">
+			<InputCheckbox {...props} />
+			<Label htmlFor={name} className="text-sm">
+				{label}
+			</Label>
+		</div>
+	);
 }
